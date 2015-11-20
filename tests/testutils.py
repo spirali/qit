b22c03d815d64cafaa155653a048be2a053cbbf3
Stanislav Bohm <stanislav.bohm@vsb.cz>
2015-11-20 13:54:23 +0100

    TEST: Cleaning build dir

--- tests/testutils.py
+++ tests/testutils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 
 TEST_DIR = os.path.dirname(__file__)
 ROOT_DIR = os.path.dirname(TEST_DIR)
@@ -12,6 +13,16 @@ def init():
     if not os.path.isdir(BUILD_DIR):
         os.makedirs(BUILD_DIR)
 
+def cleanup_build_dir():
+    if os.path.isdir(BUILD_DIR):
+        for item in os.listdir(BUILD_DIR):
+            path = os.path.join(BUILD_DIR, item)
+            if os.path.isfile(path):
+                os.unlink(path)
+            else:
+                shutil.rmtree(path)
+
 def Qit():
+    cleanup_build_dir()
     import qit
     return qit.Qit(debug=True, build_dir=BUILD_DIR)
