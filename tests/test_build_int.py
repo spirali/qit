6f16a9760ba828f44cc2ef615c0d312a8a5cbd61
Jakub Beránek <berykubik@gmail.com>
2015-11-28 12:46:54 +0100

    TEST: added tests for Range with start and step

--- tests/test_build_int.py
+++ tests/test_build_int.py
@@ -14,3 +14,21 @@ def test_range_generate():
     lst = c.run(expr)
     assert len(lst) == 10
     assert all(i >= 0 and i < 10 for i in lst)
+
+
+def test_min_range_iterate():
+    expr = Range(15, 42).iterate()
+    c = Qit()
+    assert list(range(15, 42)) == c.run(expr)
+
+def test_min_range_generate():
+    expr = Range(15, 42).generate().take(100)
+    c = Qit()
+    lst = c.run(expr)
+    assert len(lst) == 100
+    assert all(i >= 15 and i < 42 for i in lst)
+
+def test_min_step_range_iterate():
+    expr = Range(9, 18, 2).iterate()
+    c = Qit()
+    assert list(range(9, 18, 2)) == c.run(expr)
