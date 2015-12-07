f824c7ffbf11ee13ac419b12416af9805db0d727
Stanislav Bohm <stanislav.bohm@vsb.cz>
2015-12-07 09:47:40 +0100

    FIX: qint used in rand_int

--- src/qit/functions/random.py
+++ src/qit/functions/random.py
@@ -5,4 +5,4 @@ from qit.base.int import Int
 rand_int = Function("rand_int")
 rand_int.takes(Int(), "from").takes(Int(), "to")
 rand_int.returns(Int())
-rand_int.code("return std::uniform_int_distribution<int>(from, to - 1)(QIT_GENERATOR);")
+rand_int.code("return std::uniform_int_distribution<qint>(from, to - 1)(QIT_GENERATOR);")
