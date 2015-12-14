7bb02b80748c04d5e91b022c7d0d87a233b08cab
Stanislav Bohm <stanislav.bohm@vsb.cz>
2015-12-14 09:57:55 +0100

    RF: domain/image renamed to key/value ("domain" is used elsewhere)

--- src/qit/base/map.py
+++ src/qit/base/map.py
@@ -4,26 +4,26 @@ from qit.base.type import Type
 
 class Map(Type):
 
-    def __init__(self, domain_type, image_type):
+    def __init__(self, key_type, value_type):
         self.name = None
 
-        self.domain_type = domain_type
-        self.image_type = image_type
+        self.key_type = key_type
+        self.value_type = value_type
 
     @property
     def childs(self):
-        return (self.domain_type, self.image_type)
+        return (self.key_type, self.value_type)
 
     def childs_from_value(self, value):
         return tuple(v[0] for v in value) + tuple(v[1] for v in value)
 
     def build(self, builder):
-        return "std::map<{}, {} >".format(self.domain_type.build(builder),
-                                          self.image_type.build(builder))
+        return "std::map<{}, {} >".format(self.key_type.build(builder),
+                                          self.value_type.build(builder))
 
     def read(self, f):
         size = Int().read(f)
-        return dict((self.domain_type.read(f), self.image_type.read(f))
+        return dict((self.key_type.read(f), self.value_type.read(f))
                  for i in range(size))
 
     @property
@@ -35,8 +35,8 @@ class Map(Type):
             {{ key_write }}(output, it->first);
             {{ value_write }}(output, it->second);
         }
-        """, key_write=self.domain_type.write_function,
-             value_write=self.image_type.write_function,
+        """, key_write=self.key_type.write_function,
+             value_write=self.value_type.write_function,
              write_int=Int().write_function)
         return f
 
@@ -44,7 +44,7 @@ class Map(Type):
         return isinstance(obj, dict)
 
     def transform_python_instance(self, obj):
-        return frozenset((self.domain_type.value(k), self.image_type.value(v))
+        return frozenset((self.key_type.value(k), self.value_type.value(v))
                      for k, v in obj.items())
 
     def build_value(self, builder, value):
@@ -54,5 +54,5 @@ class Map(Type):
         return "{0} ({{ {1} }})".format(self.build(builder), arg)
 
     def __repr__(self):
-        return "Map({}, {})".format(repr(self.domain_type),
-                                    repr(self.image_type))
+        return "Map({}, {})".format(repr(self.key_type),
+                                    repr(self.value_type))
