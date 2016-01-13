980a99857dba4aa133acd5587e3e74be92213f94
Martin Surkovsky <martin.surkovsky@gmail.com>
2016-01-13 22:53:27 +0100

    TEST: action system

--- tests/test_system.py
+++ tests/test_system.py
@@ -1,9 +1,10 @@
 from testutils import Qit, init
 init()
 
-from qit import Int, Range, System, Domain
-from qit import Function, Product, Vector, Variable
+from qit import Int, Range, System, Domain, ActionSystem, Bool
+from qit import Function, Product, Vector, Variable, Functor, Values
 import itertools
+
 
 def test_system_empty():
     s = System((Int() * Int()).values(), ())
@@ -78,3 +79,121 @@ def test_system_rule_variable():
     s = System(Int().values(5), (f,))
     result = ctx.run(s.states(3).iterate(), args={x: 5})
     assert result == [5]
+
+inline_code = """
+    if (s1.s1_id != s2.s1_id) {
+        return s1.s1_id < s2.s1_id;
+    }
+    if (s1.action != s2.action) {
+        return s1.action < s2.action;
+    }
+    return s1.s2_id < s2.s2_id;"""
+
+def test_empty_action_system():
+
+    s = ActionSystem(Int().values(1), ())
+    result = Qit().run(s.states(10).iterate())
+    assert result == []
+
+def test_basic_action_system():
+    f = Function("f").takes(Int(), "x").returns(Int()).code("return x + 1;")
+    s = ActionSystem(Int().values(1), (f,))
+    cmp_system_states = Function().takes(s.sas_type, "s1")\
+                                  .takes(s.sas_type, "s2")\
+                                  .returns(Bool())
+    cmp_system_states.code(inline_code)
+    depth = 10
+    result = Qit().run(s.states(depth).iterate().sort(cmp_fn=cmp_system_states))
+    expected = [(i, 'f', i + 1) for i in range(1, depth + 2)]
+    assert result == expected
+
+def test_basic_action_system_depth():
+    f = Function("f").takes(Int(), "x").returns(Int()).code("return x + 1;")
+    s = ActionSystem(Int().values(1), (f,))
+    cmp_system_states = Function().takes(s.sas_type, "s1")\
+                                  .takes(s.sas_type, "s2")\
+                                  .returns(Bool())
+    cmp_system_states.code(inline_code)
+    depth = 10
+    result = Qit().run(s.states(depth, return_depth=True).iterate())
+    expected = [((i, 'f', i + 1), i - 1) for i in range(1, depth + 2)]
+    assert result == expected
+
+def test_action_system_two_functions():
+    f = Function("f").takes(Int(), "x").returns(Int()).code("return x + 2;")
+    g = Function("g").takes(Int(), "x").returns(Int()).code("return x - 1;")
+
+    s = ActionSystem(Int().values(1), (f, g))
+    result = Qit().run(s.states(2).iterate())
+    assert result == [(1, 'f', 2),
+                      (1, 'g', 3),
+                      (3, 'f', 4),
+                      (3, 'g', 5),
+                      (2, 'f', 6),
+                      (2, 'g', 4),
+                      (6, 'f', 7),
+                      (6, 'g', 8),
+                      (5, 'f', 1),
+                      (5, 'g', 9),
+                      (4, 'f', 8),
+                      (4, 'g', 1)]
+
+    cmp_system_states = Function().takes(s.sas_type, "s1")\
+                                  .takes(s.sas_type, "s2")\
+                                  .returns(Bool())
+    cmp_system_states.code(inline_code)
+    result = Qit().run(s.states(2).iterate().sort(cmp_fn=cmp_system_states))
+    assert result == [(1, 'f', 2),
+                      (1, 'g', 3),
+                      (2, 'f', 6),
+                      (2, 'g', 4),
+                      (3, 'f', 4),
+                      (3, 'g', 5),
+                      (4, 'f', 8),
+                      (4, 'g', 1),
+                      (5, 'f', 1),
+                      (5, 'g', 9),
+                      (6, 'f', 7),
+                      (6, 'g', 8)]
+
+def test_action_system_empty_vector_action():
+    empty = Function().takes(Int(), "x").returns(Vector(Int()))
+    empty.code("return {};")
+
+    s = ActionSystem(Int().values(1), (empty, ))
+    result = Qit().run(s.states(10).iterate())
+    assert result == []
+
+def test_action_system_vector_action():
+    odd = Function("odd").takes(Int(), "x").returns(Vector(Int())).code("""
+        if (x % 2 != 0) {
+            return {x, 2*x + 1};
+        }
+        return {};
+    """)
+
+    s = ActionSystem(Int().values(1), (odd, ))
+    depth = 5
+    result = Qit().run(s.states(depth).iterate())
+    expected = list(itertools.chain.from_iterable(
+        ((i, "odd", i), (i, "odd", i + 1)) for i in range(1, depth + 2)))
+    assert result == expected
+
+def test_action_system_free_variable():
+    ctx = Qit()
+
+    f = Function("f").takes(Int(), "x").returns(Int()).code("""
+        return x + 1;
+    """)
+
+    x = Int().variable("x")
+    s = ActionSystem(Values(Int(), [x]), (f, ))
+    fs = s.states(2).iterate().make_function((x,))
+
+    g = Function().takes(Int(), "x").returns(Vector(s.sas_type)).code("""
+        return {{fs}}(x);
+    """, fs=fs)
+
+    r = Range(3)
+    res = ctx.run(r.iterate().map(g))
+    assert res == [[(i, "f", i + 1) for i in range(1, 4)] for j in range(3)]
